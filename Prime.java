0228a8e5839984a36e4f6f1c2b58014cfbec8992
chayi chaithu <chaitanya>
2024-12-09 10:49:10 +0530

    one

---
+++ Prime.java
@@ -0,0 +1,29 @@
+
+class Prime{
+    public static void main (String[] args)
+    {
+        int i=1;
+        while(i>100){
+            if(prime(i))
+            {
+                System.out.println(i+"is prime");
+            }
+            else
+            {
+                System.out.println(i+"is not prime");
+            }
+
+        }
+    }
+    private static boolean prime(int n)
+    {
+        if(n<=1)
+        return false;
+        for(int j=2;j<n*n;j++)
+        {
+            if(j%n==0)
+            return false;
+        }
+        return true;
+    }
+}
