f896063fb8bf2542ce2e89d9bcee26f9722209ed
chayi chaithu <chaitanya>
2024-12-09 10:53:10 +0530

    oneee

--- Prime.java
+++ Prime.java
@@ -3,7 +3,7 @@ class Prime{
     public static void main (String[] args)
     {
         int i=1;
-        while(i>100){
+        while(i<100){
             if(prime(i))
             {
                 System.out.println(i+"is prime");
